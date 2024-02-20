e025b682fd9c7d2ce2bb71453892ab4530bbb492
Manoj Reddy <manojreddygangarapu@gmail.com>
2024-02-20 18:29:19 +0530

    firebase test lab integration testing example

--- android_integration.sh
+++ android_integration.sh
@@ -1,30 +1,15 @@
+flutter build apk --debug 
+
 pushd android
-# flutter build generates files in android/ for building the app
-flutter build apk
-./gradlew app:assembleAndroidTest
-./gradlew app:assembleDebug -Ptarget=integration_test/app_test.dart
+    ./gradlew app:assembleDebugAndroidTest
+    ./gradlew app:assembleDebug -Ptarget=`pwd`/../integration_test/app_test.dart
+    #./gradlew app:connectedDebugAndroidTest -Ptarget=integration_test/tests/create_todo_test.dart   #- To Run Test On Local For Verification
 popd
 
 gcloud auth activate-service-account --key-file=prism-dev-030122-75e34e02152f.json
 gcloud --quiet config set project prism-dev-030122
 
 gcloud firebase test android run --type instrumentation \
-  --app build/app/outputs/apk/debug/app-debug.apk \
-  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk\
-  --use-orchestrator \
-  # --device model=panther \
-  --timeout 5m \
-  --results-bucket=gs://prism-dev-030122.appspot.com
-
-## > gcloud firebase test android models list
-# gcloud firebase test android run --type instrumentation \
-#   --app build/app/outputs/apk/debug/app-debug.apk \
-#   --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
-#   --device-ids=blueline,OnePlus5T,x1q,redfin,flame \
-#   --os-version-ids=25,26,27,28,29,30 \
-#   --locales=en_GB,es \
-#   --orientations=portrait,landscape \
-#   --use-orchestrator \
-#   --timeout 40m \
-#   --results-bucket=gs://integration_results_mjcoffee \
-#   --results-dir=tests/firebase  
+    --app build/app/outputs/flutter-apk/app-debug.apk \
+    --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+    --timeout 5m
--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -5,7 +5,7 @@ import 'package:integration_test/integration_test.dart';
 import 'package:prism_integration_testing/main.dart' as tests;
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets(
       'Verify navigation from the onboarding and authentication screens to the home screen.',
@@ -20,6 +20,13 @@ void main() {
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pumpAndSettle();
+
+    // This is required prior to taking the screenshot (Android only).
+    await binding.convertFlutterSurfaceToImage();
+
+    // Trigger a frame.
+    await tester.pumpAndSettle();
+    await binding.takeScreenshot('screenshot-1');
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
